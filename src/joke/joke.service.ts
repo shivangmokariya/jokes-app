627f9fca4069d26d2e20032ff077fb7f1e2862ed
shivang mokariya <shivangmokariya.dev@gmail.com>
2023-12-13 22:38:01 +0530

    made changes

--- src/joke/joke.service.ts
+++ src/joke/joke.service.ts
@@ -10,7 +10,7 @@ export class jokeService {
     const apiUrl = process.env.NORRIS_API_URL;
     try {
       const response = await axios.get(apiUrl);
-      const joke = response.data.value;
+      const {value : joke}=response.data;
       return {message:'Your random joke fetched successfully', joke };
     } catch (error) {
       console.error('Error fetching Data:', error.message);
